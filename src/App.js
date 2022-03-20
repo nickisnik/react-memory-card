6e3bbf4b7077442aa6ea61bf6308882df254f88f
Nick <nikita.nikolenko@gmail.com>
2022-03-20 23:13:00 +0000

    final_

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Card from './components/Card'
 
+// NEED TO ADD INDIVIDUAL KEYS TO CARD ARRAY
+
 function App() {
 
   const [cardList, setCardList] = useState(['A', 'O', 'U', 'F', 'E', 'Y', 'B', 'I']);
@@ -10,6 +12,7 @@ function App() {
   const [score, setScore] = useState(0);
   const [lastScore, setLastScore] = useState(0);
   const [wrongCard, setWrongCard] = useState('');
+  const [winAnnounce, setWinAnnounce] = useState(false);
 
   function shuffle() {
     const cardArray = [...cardList];
@@ -22,10 +25,12 @@ function App() {
   }
 
   function gameOver() {
+    setWinAnnounce(false);
     setWrongCard('');
     setLastScore(score);
     setScore(0);
     setUsedCards([]);
+    shuffle()
   }
   function handleClick(e) {
     const cardTarget = e.target.querySelector('span').textContent;
@@ -37,11 +42,24 @@ function App() {
 
     setUsedCards([...usedCards, cardTarget])
     setScore(score + 1);
+    shuffle()
   }
 
   useEffect(() => {
-    shuffle()
-  }, [score])
+    // ON PAGE LOAD
+    shuffle();
+  }, [])
+
+  useEffect(() => {
+    if(score > 7) {
+      setWinAnnounce(true);
+      setTimeout(gameOver, 1500);
+    }
+  }, [score]);
+  
+  // useEffect(() => {
+  //   shuffle()
+  // }, [score])
 
   
   const cards = cardList.map((item, index) => {
@@ -57,9 +75,11 @@ function App() {
       <header>
         <span>Memory Card</span>
       </header>
-
       <section className='card-container'>
         {cards}
+        <div className={winAnnounce ? 'win-overlay show' : 'win-overlay'}>
+          <span>You win!</span>
+        </div>
       </section>
       <div className='scores'>
         <span>Current score: {score}</span>
